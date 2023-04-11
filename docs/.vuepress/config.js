3529ff53c816ea062530fcb20727cc4e4606abfe
yinca <727929899@qq.com>
2023-04-12 00:23:53 +0800

    demo

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    themeConfig: {
+      logo: '/assets/img/login.jpg',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: '后端', link: '/guide/' },
+        { text: '前端', link: '/xx/' },
+        { text: 'GitHub', link: 'https://github.com/xiechuanan/VuePress' },
+      ]
+    }
+  }
