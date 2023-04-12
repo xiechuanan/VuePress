32de0d5085543e81797e91fa56070c7bf1c709a6
yin <727929899@qq.com>
2023-04-12 17:03:16 +0800

    新增导航栏文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,27 @@
 module.exports = {
-    themeConfig: {
-      logo: '/assets/img/login.jpg',
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: '后端', link: '/guide/' },
-        { text: '前端', link: '/xx/' },
-        { text: 'GitHub', link: 'https://github.com/xiechuanan/VuePress' },
-      ]
-    }
-  }
+  themeConfig: {
+    logo: "/assets/img/login.jpg",
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "后端开发",
+        ariaLabel: "后端",
+        items: [
+          { text: "PHP", link: "/后端/PHP/" },
+          { text: "JAVA", link: "/后端/JAVA/" },
+        ],
+      },
+      { text: "前端开发", link: "/前端/" },
+      { text: "移动端开发", link: "/移动端/" },
+      { text: "数据结构与算法", link: "/数据结构与算法/" },
+      { text: "GitHub", link: "https://github.com/xiechuanan/VuePress" },
+    ],
+
+    sidebar: {
+      "/后端/JAVA/": ['','基础'],
+
+      // fallback
+      "/": ["about"],
+    },
+  },
+};
